8197c06cce010a42a23c5c7c395ffc89eb45d678
szafqupl <garfieldowce1@gmail.com>
2025-10-04 13:22:56 +0200

    Add files via upload

---
+++ channels.js
@@ -0,0 +1,582 @@
+/* Generated from CDA TV.txt */
+const CHANNELS = [
+  {
+    "name": "TVP1 HD",
+    "mpd": "https://liveovh011.cda.pl/enc115/tvp1hdraw/tvp1hdraw.mpd"
+  },
+  {
+    "name": "TVP2 HD",
+    "mpd": "https://liveovh010.cda.pl/enc115/tvp2hdraw/tvp2hdraw.mpd"
+  },
+  {
+    "name": "Polsat HD",
+    "mpd": "https://lb2-e2-19.pluscdn.pl/ch/1502600/308/dash/20a18c30/live.mpd?uid=2253542221"
+  },
+  {
+    "name": "TV4",
+    "mpd": "https://cdn-s-lb2.pluscdn.pl/ch/1502601/309/dash/e25c2c93/live.mpd?uid=2253542221"
+  },
+  {
+    "name": "TVPuls",
+    "mpd": "https://liveovh011.cda.pl/enc030/tvpuls/tvpuls.mpd?uid=2253542221",
+    "clearKeys": {
+      "65346136356161323237353738393133": "34386266366363383037323536373666"
+    }
+  },
+  {
+    "name": "Puls 2",
+    "mpd": "https://liveovh011.cda.pl/enc103/puls2/puls2.mpd?uid=2253542221",
+    "clearKeys": {
+      "35333962663265373936353465333161": "65666565366566663937393430316234"
+    }
+  },
+  {
+    "name": "Viasat True Crime",
+    "mpd": "https://liveovh011.cda.pl/enc106/viasattruecrime/viasattruecrime.mpd?uid=2253542221",
+    "clearKeys": {
+      "36626465666163383637343437316535": "66663834343563313632356534313435"
+    }
+  },
+  {
+    "name": "Polsat Crime + Investigation HD",
+    "mpd": "https://liveovh011.cda.pl/enc107/polsatcihd/polsatcihd.mpd?uid=2253542221",
+    "clearKeys": {
+      "38303761623335366563653830376530": "37343766646562323366346433346235"
+    }
+  },
+  {
+    "name": "HOME TV HD",
+    "mpd": "https://liveovh009.cda.pl/enc124/hometvhd/hometvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36396433303634626432386130303665": "31313763363666366630313138356630"
+    }
+  },
+  {
+    "name": "BBC Lifestyle HD",
+    "mpd": "https://liveovh009.cda.pl/enc114/bbclifestylehd/bbclifestylehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "64623932626531626333363031333036": "62383234393737353666666634643134"
+    }
+  },
+  {
+    "name": "CBS Reality HD",
+    "mpd": "https://liveovh011.cda.pl/enc121/cbsrealityhd/cbsrealityhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "35373631383766373536623732303936": "63396663626563363366643630383362"
+    }
+  },
+  {
+    "name": "DocuBox HD",
+    "mpd": "https://liveovh010.cda.pl/enc110/docuboxhd/docuboxhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36363139333137623039333863383536": "63383638646339323263363630363962"
+    }
+  },
+  {
+    "name": "HISTORY HD",
+    "mpd": "https://liveovh010.cda.pl/enc102/historyhd/historyhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "38616337643132623663353564303662": "32393338643863616135666231663735"
+    }
+  },
+  {
+    "name": "HISTORY2 HD",
+    "mpd": "https://liveovh009.cda.pl/enc102/history2hd/history2hd.mpd?uid=2253542221",
+    "clearKeys": {
+      "33313063333263303565633663396463": "37646232363932633333646161356462"
+    }
+  },
+  {
+    "name": "Polsat Viasat History HD",
+    "mpd": "https://liveovh009.cda.pl/enc105/polsatviasathistoryhd/polsatviasathistoryhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "30616234666135626234663833656332": "32346162383465353366343662303230"
+    }
+  },
+  {
+    "name": "Polsat Viasat Explorer HD",
+    "mpd": "https://liveovh009.cda.pl/enc107/polsatviasatexplorehd/polsatviasatexplorehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "39316230306133613038376563323232": "37643833656263356238633839326164"
+    }
+  },
+  {
+    "name": "Polsat Viasat Nature HD",
+    "mpd": "https://liveovh009.cda.pl/enc104/polsatviasatnaturehd/polsatviasatnaturehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "66383331326437333761643231353431": "65613632646133353232353265633132"
+    }
+  },
+  {
+    "name": "BBC Earth",
+    "mpd": "https://liveovh009.cda.pl/enc126/bbcearthhd/bbcearthhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37383832326334323266636639613866": "38366563336532633837653863306639"
+    }
+  },
+  {
+    "name": "National Geographic Wild HD",
+    "mpd": "https://liveovh009.cda.pl/enc104/nationalgeographicwildhd/nationalgeographicwildhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "61393330653239376330646231333664": "32613464313835636364336337383566"
+    }
+  },
+  {
+    "name": "Nat Geo People HD",
+    "mpd": "https://liveovh011.cda.pl/enc101/natgeopeoplehd/natgeopeoplehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "39356566333132653635323630633239": "63616433343937646535353634393032"
+    }
+  },
+  {
+    "name": "National Geographic HD",
+    "mpd": "https://liveovh011.cda.pl/enc104/nationalgeographichd/nationalgeographichd.mpd?uid=2253542221",
+    "clearKeys": {
+      "35623732393930653339383464343762": "66333934616435653530333364346339"
+    }
+  },
+  {
+    "name": "BBC First HD",
+    "mpd": "https://liveovh011.cda.pl/enc113/bbcfirsthd/bbcfirsthd.mpd?uid=2253542221",
+    "clearKeys": {
+      "30653831663631363739326336373639": "36666431646535316531353965383931"
+    }
+  },
+  {
+    "name": "BBC Brit HD",
+    "mpd": "https://liveovh010.cda.pl/enc116/bbcbrithd/bbcbrithd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36336231393930663561666231363136": "62383939616433346463353765333138"
+    }
+  },
+  {
+    "name": "FILMAX HD",
+    "mpd": "https://liveovh010.cda.pl/enc113/filmaxhd/filmaxhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36343463643438333461663161333465": "66656430306232653730663534396163"
+    }
+  },
+  {
+    "name": "Epic Drama HD",
+    "mpd": "https://liveovh009.cda.pl/enc119/viasatepicdramahd/viasatepicdramahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "33376132393239396134643938643532": "63666138313065636530326436343936"
+    }
+  },
+  {
+    "name": "KINO TV",
+    "mpd": "https://liveovh010.cda.pl/enc110/kinotvhd/kinotvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "35653633346639343338613365313564": "63393733666337666534636263326136"
+    }
+  },
+  {
+    "name": "Kino Polska HD",
+    "mpd": "https://liveovh010.cda.pl/enc105/kinopolskahd/kinopolskahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "39613761333935366330613239363836": "66336466646639316130303261616433"
+    }
+  },
+  {
+    "name": "Filmbox Premium",
+    "mpd": "https://liveovh009.cda.pl/enc105/filmboxpremiumhd/filmboxpremiumhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36343432666531333137343033353337": "31383230306164363436656635306462"
+    }
+  },
+  {
+    "name": "Filmbox Extra",
+    "mpd": "https://liveovh010.cda.pl/enc107/filmboxextrahd/filmboxextrahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "64306635383231333538663962316663": "32613032646462663937306634643732"
+    }
+  },
+  {
+    "name": "Filmbox Action",
+    "mpd": "https://liveovh009.cda.pl/enc108/filmboxaction/filmboxaction.mpd?uid=2253542221",
+    "clearKeys": {
+      "34633034383761663262386666346566": "66363933383231663938366633393535"
+    }
+  },
+  {
+    "name": "ArtHouse HD",
+    "mpd": "https://liveovh010.cda.pl/enc111/arthousehd/arthousehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "31616436396461383735663561646562": "64303735343361373961373836643761"
+    }
+  },
+  {
+    "name": "Filmbox Family",
+    "mpd": "https://liveovh009.cda.pl/enc108/filmboxfamily/filmboxfamily.mpd?uid=2253542221",
+    "clearKeys": {
+      "30386231313461623035353334393863": "37343966396361336539356463616336"
+    }
+  },
+  {
+    "name": "Film Cafe",
+    "mpd": "https://liveovh010.cda.pl/enc121/cbseuropahd/cbseuropahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36396430306161623231373566386266": "37653464363062396635623036373765"
+    }
+  },
+  {
+    "name": "AMC HD",
+    "mpd": "https://liveovh009.cda.pl/enc119/amchd/amchd.mpd?uid=2253542221",
+    "clearKeys": {
+      "39333163663866653130653066323731": "34393361356161333638386232366132"
+    }
+  },
+  {
+    "name": "Bollywood HD",
+    "mpd": "https://liveovh010.cda.pl/enc112/studiomedtvhd/studiomedtvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37316463343232333733613633356234": "31363332633565343636653166663735"
+    }
+  },
+  {
+    "name": "Sundance HD",
+    "mpd": "https://liveovh010.cda.pl/enc108/sundancehd/sundancehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "35366366366238626630303364393961": "37393531343435356532316531363134"
+    }
+  },
+  {
+    "name": "Romance TV",
+    "mpd": "https://liveovh009.cda.pl/enc116/romancetvhd/romancetvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "61343434316132393632613333373934": "30366263373233356330643633343861"
+    }
+  },
+  {
+    "name": "Red Carpet TV HD",
+    "mpd": "https://liveovh011.cda.pl/enc105/redcarpettvhd/redcarpettvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "61333335653665316663303965306662": "36343365303062393066366337303739"
+    }
+  },
+  {
+    "name": "Vi DoCTV",
+    "mpd": "https://liveovh010.cda.pl/enc022/redtoptvhd/redtoptvhd.mpd?uid=12277377c",
+    "clearKeys": {
+      "32333331376335646530626433643532": "36393632363731653032376166643532"
+    }
+  },
+  {
+    "name": "STOPKLATKA TV",
+    "mpd": "https://liveovh011.cda.pl/enc119/biznesowatv/biznesowatv.mpd?uid=12277377c",
+    "clearKeys": {
+      "35363632323838356665333236333633": "66336338313735666636363861363231"
+    }
+  },
+  {
+    "name": "2X2 HD",
+    "mpd": "https://liveovh011.cda.pl/enc117/2x2hd/2x2hd.mpd?uid=2253542221",
+    "clearKeys": {
+      "32393330346432653934323037323533": "66666265623435666233393130646362"
+    }
+  },
+  {
+    "name": "Disney Channel HD",
+    "mpd": "https://liveovh010.cda.pl/enc103/disneychannelhd/disneychannelhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "32343732323864363530376465333366": "34366362643431306661383832396136"
+    }
+  },
+  {
+    "name": "Top Kids HD",
+    "mpd": "https://liveovh010.cda.pl/enc116/topkidshd/topkidshd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37343466613537336539633033333962": "31626232353536303030646531343230"
+    }
+  },
+  {
+    "name": "Polsat JimJam",
+    "mpd": "https://liveovh010.cda.pl/enc119/polsatjimjam/polsatjimjam.mpd?uid=2253542221",
+    "clearKeys": {
+      "30333434306136616662376464633432": "36366366333561316264666531393632"
+    }
+  },
+  {
+    "name": "Junior Music",
+    "mpd": "https://liveovh011.cda.pl/enc117/topkidsjrhd/topkidsjrhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "33386239383965386334336364333339": "34386637386634376663376335313232"
+    }
+  },
+  {
+    "name": "Cbeebies",
+    "mpd": "https://liveovh009.cda.pl/enc126/bbccbeebies/bbccbeebies.mpd?uid=2253542221",
+    "clearKeys": {
+      "65346637386464346161363833396466": "31356662643164333861633861623634"
+    }
+  },
+  {
+    "name": "DuckTV HD",
+    "mpd": "https://liveovh010.cda.pl/enc112/ducktvhd/ducktvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "66663739336631613533303339323538": "39303466363464646231303965333564"
+    }
+  },
+  {
+    "name": "DuckTV Plus HD",
+    "mpd": "https://liveovh010.cda.pl/enc112/ducktvplushd/ducktvplushd.mpd?uid=2253542221",
+    "clearKeys": {
+      "64303962316132333361386634333662": "61303763366335363264393538643866"
+    }
+  },
+  {
+    "name": "Da Vinci",
+    "mpd": "https://liveovh009.cda.pl/enc109/davincihd/davincihd.mpd?uid=12277377c",
+    "clearKeys": {
+      "37633362303365313437313164393734": "38393231653430383163373062643731"
+    }
+  },
+  {
+    "name": "GAMETOON HD",
+    "mpd": "https://liveovh011.cda.pl/enc122/gametoonhd/gametoonhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "64623638313331666436363261646134": "64383733626163656564653035633438"
+    }
+  },
+  {
+    "name": "TVC HD",
+    "mpd": "https://liveovh010.cda.pl/enc118/tvchd/tvchd.mpd?uid=2253542221",
+    "clearKeys": {
+      "38353634323036303463303763306631": "34623930623930383163383734666634"
+    }
+  },
+  {
+    "name": "Antena TV HD",
+    "mpd": "https://liveovh010.cda.pl/enc114/antenatvhd/antenatvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37656461646632623064326238376531": "36643837623433376562653234353831"
+    }
+  },
+  {
+    "name": "Zoom TV",
+    "mpd": "https://liveovh010.cda.pl/enc125/zoomtvhd/zoomtvhd.mpd?uid=12277377c",
+    "clearKeys": {
+      "32323265373563363464623237633761": "38323461633034326234373539336231"
+    }
+  },
+  {
+    "name": "WP HD",
+    "mpd": "https://liveovh009.cda.pl/enc109/wphd/wphd.mpd?uid=12277377c",
+    "clearKeys": {
+      "37326531373361376636373433373363": "66643462666134373235616438316438"
+    }
+  },
+  {
+    "name": "Sportowa TV",
+    "mpd": "https://liveovh010.cda.pl/enc011/sportowatv/sportowatv.mpd?uid=12277377c",
+    "clearKeys": {
+      "32353330373035383831306533316635": "62393839616439313237373764656264"
+    }
+  },
+  {
+    "name": "Xtreme TV",
+    "mpd": "https://liveovh011.cda.pl/enc127/supertvhd/supertvhd.mpd?uid=12277377c",
+    "clearKeys": {
+      "63393731373764323532303039386662": "39336530353337633037376137306562"
+    }
+  },
+  {
+    "name": "Extreme Sports Channel",
+    "mpd": "https://liveovh010.cda.pl/enc124/extremesportshd/extremesportshd.mpd?uid=2253542221",
+    "clearKeys": {
+      "62323334393638386230623537333130": "61633430613062363961333266313835"
+    }
+  },
+  {
+    "name": "FightBox HD",
+    "mpd": "https://liveovh010.cda.pl/enc111/fightboxhd/fightboxhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36373039373534343835363630353765": "63623335343764353336643635333531"
+    }
+  },
+  {
+    "name": "Motowizja HD",
+    "mpd": "https://liveovh010.cda.pl/enc116/motowizjahd/motowizjahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37373830396161643832373531666433": "62633331373039346661353231663062"
+    }
+  },
+  {
+    "name": "StudioMed TV HD",
+    "mpd": "https://liveovh010.cda.pl/enc112/studiomedtvhd/studiomedtvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "37316463343232333733613633356234": "31363332633565343636653166663735"
+    }
+  },
+  {
+    "name": "TVS",
+    "mpd": "https://liveovh010.cda.pl/enc118/tvs/tvs.mpd?uid=2253542221",
+    "clearKeys": {
+      "31633562636139303238336561643738": "33313062613932633464303162303564"
+    }
+  },
+  {
+    "name": "TVT",
+    "mpd": "https://liveovh010.cda.pl/enc117/tvt/tvt.mpd?uid=12277377c",
+    "clearKeys": {
+      "39663463353338666232663335396131": "33346336626134383639666164653539"
+    }
+  },
+  {
+    "name": "TV Toya",
+    "mpd": "https://liveovh009.cda.pl/enc130/toyatv/toyatv.mpd?uid=12277377c",
+    "clearKeys": {
+      "38363965616466633232326462303434": "64383966386434633836343535313632"
+    }
+  },
+  {
+    "name": "CTV Dla Ciebie",
+    "mpd": "https://liveovh010.cda.pl/enc026/ctvdlaciebie/ctvdlaciebie.mpd?uid=12277377c",
+    "clearKeys": {
+      "38636134313631643839376638373935": "63303165323136663266326332633264"
+    }
+  },
+  {
+    "name": "Twoja.TV HD",
+    "mpd": "https://liveovh010.cda.pl/enc113/twojatvhd/twojatvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "34353833366235333766633135653130": "35643833356539366363616433316133"
+    }
+  },
+  {
+    "name": "Echo 24",
+    "mpd": "https://liveovh009.cda.pl/enc027/echo24hd/echo24hd.mpd?uid=12277377c",
+    "clearKeys": {
+      "37666433306132303632386435653033": "37396363613663643138346438363865"
+    }
+  },
+  {
+    "name": "TV REGIO",
+    "mpd": "https://liveovh011.cda.pl/enc114/tvregiohd/tvregiohd.mpd?uid=12277377c",
+    "clearKeys": {
+      "34333738326462383733633962356531": "66653234303166613761393236623936"
+    }
+  },
+  {
+    "name": "Telewizja Biznesowa",
+    "mpd": "https://liveovh011.cda.pl/enc119/biznesowatv/biznesowatv.mpd?uid=12277377c",
+    "clearKeys": {
+      "62366132393735666132316338306163": "66373931346662623138636631633031"
+    }
+  },
+  {
+    "name": "Biznes24 HD",
+    "mpd": "https://liveovh011.cda.pl/enc109/biznes24hd/biznes24hd.mpd?uid=12277377c",
+    "clearKeys": {
+      "30633731316664633565383932383433": "37356235376638613263343834363638"
+    }
+  },
+  {
+    "name": "wPolsce24",
+    "mpd": "https://liveovh011.cda.pl/enc110/wpolsceplhd/wpolsceplhd.mpd?uid=12277377c",
+    "clearKeys": {
+      "35633130636639316364383436346466": "39313938633537373961623262383765"
+    }
+  },
+  {
+    "name": "News24",
+    "mpd": "https://liveovh009.cda.pl/enc118/news24hd/news24hd.mpd?uid=12277377c",
+    "clearKeys": {
+      "33666436346162383666623362373665": "61356232393934653266323635333563"
+    }
+  },
+  {
+    "name": "TVP3 Warszawa",
+    "mpd": "https://liveovh010.cda.pl/enc030/tvp3warszawa/tvp3warszawa.mpd",
+    "clearKeys": {
+      "37363965663339613939333866303765": "31303738386232653731376432626234"
+    }
+  },
+  {
+    "name": "Telewizja Republika",
+    "mpd": "https://liveovh010.cda.pl/enc010/telewizjarepublika/telewizjarepublika.mpd?uid=2253542221",
+    "clearKeys": {
+      "35363537313533343030393837383361": "35356131646236376432616463643538",
+      "37363965663339613939333866303765": "31303738386232653731376432626234",
+      "34323632633038306231313430373964": "30613337363239396537323461666338",
+      "32343732323864363530376465333366": "34366362643431306661383832396136"
+    }
+  },
+  {
+    "name": "TV Republika HD",
+    "mpd": "https://liveovh009.cda.pl/enc120/tvrepublikahd/tvrepublikahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36643337373565356635383232326534": "63653137323030313139363063333932"
+    }
+  },
+  {
+    "name": "Trwam TV",
+    "mpd": "https://liveovh010.cda.pl/enc117/tvrwam/tvrwam.mpd?uid=12277377c",
+    "clearKeys": {
+      "34323632633038306231313430373964": "30613337363239396537323461666338"
+    }
+  },
+  {
+    "name": "TBN Polska",
+    "mpd": "https://liveovh010.cda.pl/enc118/tbnpolska/tbnpolska.mpd?uid=12277377c",
+    "clearKeys": {
+      "62363161653866323432306466633936": "31363130656436336531383663316664"
+    }
+  },
+  {
+    "name": "SBN",
+    "mpd": "https://liveovh011.cda.pl/enc011/sbn/sbn.mpd?uid=12277377c",
+    "clearKeys": {
+      "39383533326436613564623264326366": "34313732396636643362313963333836"
+    }
+  },
+  {
+    "name": "JAZZ TV HD",
+    "mpd": "https://liveovh010.cda.pl/enc121/jazztvhd/jazztvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "61653764666632333263343463343534": "66323165623366323361653932303335"
+    }
+  },
+  {
+    "name": "MixTape HD",
+    "mpd": "https://liveovh009.cda.pl/enc122/mixtapehd/mixtapehd.mpd?uid=2253542221",
+    "clearKeys": {
+      "34616234363139616339343431333437": "63623438303436336165636232363565"
+    }
+  },
+  {
+    "name": "Music Box Polska HD",
+    "mpd": "https://liveovh010.cda.pl/enc125/musicboxpolskahd/musicboxpolskahd.mpd?uid=2253542221",
+    "clearKeys": {
+      "65333166666538393630306335323261": "39646438373762373934316461613837"
+    }
+  },
+  {
+    "name": "Kino Polska Muzyka",
+    "mpd": "https://liveovh010.cda.pl/enc122/kinopolskamuzyka/kinopolskamuzyka.mpd?uid=2253542221",
+    "clearKeys": {
+      "61613566616439336664363130343935": "30663836376237626664373862346264"
+    }
+  },
+  {
+    "name": "Power TV HD",
+    "mpd": "https://liveovh010.cda.pl/enc120/powertvhd/powertvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "64626638336639343661613336323137": "64313761383631363433343765326236"
+    }
+  },
+  {
+    "name": "NUTA TV HD",
+    "mpd": "https://liveovh011.cda.pl/enc120/nutatvhd/nutatvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "31356662636264353035316635306536": "38353735666535336531373033313835"
+    }
+  },
+  {
+    "name": "NUTA GOLD",
+    "mpd": "https://liveovh011.cda.pl/enc120/nutatvhd/nutatvhd.mpd?uid=2253542221",
+    "clearKeys": {
+      "36623366333066353064363761653038": "30376666616563313961393363336665"
+    }
+  }
+];
